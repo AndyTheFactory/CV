0e4cb794b14f442773495a8bb242642434f799be
Andrei <andrei@thephpfactory.com>
2018-12-14 18:08:12 +0200

    b

--- CVLab5/src/cvlab5/CVLab5.java
+++ CVLab5/src/cvlab5/CVLab5.java
@@ -9,6 +9,7 @@ import static cvlab5.CannyTask.file;
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -32,18 +33,30 @@ public class CVLab5 {
         //CannyTask.doTask();                
         
         
-        Ransac rans=new Ransac(30, 5, 0.03);
+        Ransac rans=new Ransac(100, 5, 20,50);
         
-        File file=new File("d:\\Work\\Java\\CV\\CVLab5\\images\\Testimg1_edge1.jpg");
+        File file=new File("images\\Testimg5_edge2.jpg");
         
         BufferedImage image=ImageIO.read(file);              
         rans.doRansac(image);
-        rans.drawLines(image);
 
-        Lab5Helper.writeImageFile(image, file, "rans1");
-        
-        
+        File file2=new File("images\\Testimg5.jpg");
+        BufferedImage image2=ImageIO.read(file2);              
+        rans.drawLines(image2);
 
+        
+        Graphics2D g2d = image2.createGraphics();
+        g2d.setColor(Color.yellow);
+        g2d.setPaint(Color.yellow);
+        BasicStroke bs = new BasicStroke(5);
+        g2d.setStroke(bs);    
+        
+        for(Rectangle r:rans.findRectangles()){
+            g2d.drawRect((int)r.getMinX(),(int)r.getMaxY(), (int)r.getWidth(), (int)r.getHeight());
+        }
+
+        Lab5Helper.writeImageFile(image2, file, "rans1");
+        
       
     }
     
--- CVLab5/src/cvlab5/Ransac.java
+++ CVLab5/src/cvlab5/Ransac.java
@@ -9,6 +9,7 @@ import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Point;
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.awt.geom.Line2D;
 import java.util.ArrayList;
@@ -22,14 +23,18 @@ public class Ransac {
     int threshold;
     double inliers;
     ArrayList<Line2D> lines;
-    static int SAMPLEWIDTH=100;
+    int SAMPLEWIDTH;
+    static int ANGLE_TOLERANCE=10;
+    static int DISTANCE_TOLERANCE=15;
     
-    public Ransac(int iterations, int threshold, double inliers)
+    
+    public Ransac(int iterations, int threshold, double inliers,int sampleWidth)
     {
         this.inliers=inliers;
         this.threshold=threshold;
         this.iterations=iterations;
-        lines=new ArrayList<>();
+        this.lines=new ArrayList<>();
+        this.SAMPLEWIDTH=sampleWidth;
     }
     
     public void doRansac(BufferedImage img)
@@ -47,19 +52,33 @@ public class Ransac {
         }
         lines.add(new Line2D.Double(minP, maxP));
     }
+    private double costFunc(Line2D line, Point p){
+        double m=(line.getY2()-line.getY1())/(line.getX1()==line.getX2()?0.00001:(line.getX2()-line.getX1())),
+                b=line.getY1();
+        return 
+                Math.min(
+                    Math.pow(
+                        p.y-m*p.x-line.getY1()
+                    ,2),
+                    Math.pow(
+                        p.x-(m==0?0.0001:(1/m))*p.y-line.getX1()
+                    ,2)
+                );
+    }
     public void Ransac(int x,int y, BufferedImage img){
         ArrayList<Point> candidates=new ArrayList<>();
         for(int i=x;i<Math.min(x+SAMPLEWIDTH, img.getWidth()-1);i++)
             for(int j=y;j<Math.min(y+SAMPLEWIDTH, img.getHeight()-1);j++)
                 if (img.getRGB(i,j)==Color.WHITE.getRGB())
                     candidates.add(new Point(i, j));
-        if (candidates.size()<=0)
+        if (candidates.size()<=2)
             return;
-        int iter=0;
+        int iter;
         double bestfit=999999999;
         Point p1,p2;
         ArrayList<Point> bestmodel=new ArrayList<>();
         do {
+            iter=0;
             bestmodel.clear();
             while (iter<iterations){
                 ArrayList<Point> inlier=new ArrayList<>();
@@ -73,12 +92,12 @@ public class Ransac {
                     double dist=model.ptLineDist(p);
                     if (dist<threshold){
                         inlier.add(p);
-                        cost+=dist;
+                        cost+=costFunc(model, p);
+                        //cost+=dist/candidates.size();
                     }
                 }
-                if (inlier.size()>candidates.size()*inliers){
+                if (inlier.size()>inliers){
                     //model fits
-                    cost=cost/inlier.size();
                     if (cost<bestfit){
                         bestfit=cost;
                         bestmodel.clear();
@@ -97,13 +116,13 @@ public class Ransac {
                 addToLines(bestmodel);
 
             }
-        } while(bestmodel.size()>0);
+        } while(bestmodel.size()>0 && candidates.size()>1);
     }
     public void drawLines(BufferedImage img){
         Graphics2D g2d = img.createGraphics();
         g2d.setColor(Color.red);
         g2d.setPaint(Color.red);
-        BasicStroke bs = new BasicStroke(2);
+        BasicStroke bs = new BasicStroke(4);
         g2d.setStroke(bs);    
         
         for(Line2D line:lines){
@@ -111,4 +130,93 @@ public class Ransac {
         }
         
     }
+    
+    public ArrayList<Rectangle> findRectangles()
+    {
+        ArrayList<Rectangle> res=new ArrayList<>();
+        for (Line2D line:lines){
+            ArrayList<Line2D> candidati=new ArrayList<Line2D>(lines);
+            candidati.remove(line);
+            
+            Line2D muchie1=findNextMuchie(line, candidati);
+            if (muchie1==null) continue;
+            
+            Line2D muchie2=findNextMuchie(line, candidati);
+            if (muchie2==null) continue;
+            
+            Line2D muchie3=findNextMuchie(line, candidati);
+            if (muchie3==null) continue;
+            
+            Line2D muchie4=findNextMuchie(line, candidati);
+            if (muchie4==null) continue;
+            double x1=Math.min(
+                    Math.min(
+                        Math.min(muchie1.getX1(), muchie1.getX2()),
+                        Math.min(muchie2.getX1(), muchie2.getX2())
+                    ),
+                    Math.min(
+                        Math.min(muchie3.getX1(), muchie3.getX2()),
+                        Math.min(muchie4.getX1(), muchie4.getX2())
+                    )
+            );
+            double x2=Math.max(
+                    Math.max(
+                        Math.max(muchie1.getX1(), muchie1.getX2()),
+                        Math.max(muchie2.getX1(), muchie2.getX2())
+                    ),
+                    Math.max(
+                        Math.max(muchie3.getX1(), muchie3.getX2()),
+                        Math.max(muchie4.getX1(), muchie4.getX2())
+                    )
+            );
+            double y1=Math.min(
+                    Math.min(
+                        Math.min(muchie1.getY1(), muchie1.getY2()),
+                        Math.min(muchie2.getY1(), muchie2.getY2())
+                    ),
+                    Math.min(
+                        Math.min(muchie3.getY1(), muchie3.getY2()),
+                        Math.min(muchie4.getY1(), muchie4.getY2())
+                    )
+            );
+            double y2=Math.max(
+                    Math.max(
+                        Math.max(muchie1.getY1(), muchie1.getY2()),
+                        Math.max(muchie2.getY1(), muchie2.getY2())
+                    ),
+                    Math.max(
+                        Math.max(muchie3.getY1(), muchie3.getY2()),
+                        Math.max(muchie4.getY1(), muchie4.getY2())
+                    )
+            );
+            
+            res.add(new Rectangle((int)x1,(int)y2,(int)(x2-x1),(int)(y2-y1)));
+            
+            
+        }
+        return res;
+    }
+    private Line2D findNextMuchie(Line2D fromline, ArrayList<Line2D> candidati)
+    {
+        for(Line2D line:lines){
+            if ((fromline.getP2().distance(line.getP1())<=DISTANCE_TOLERANCE) ||
+                (fromline.getP2().distance(line.getP2())<=DISTANCE_TOLERANCE)){
+                double angle=getAngle(fromline, line);
+                if (Math.abs(90-angle)<=ANGLE_TOLERANCE)
+                    return line;
+                
+            }
+        }
+        return null;
+    }
+    
+    private double getAngle(Line2D line1, Line2D line2){
+        double angle1 = Math.atan2(line1.getY1() - line1.getY2(),
+                                   line1.getX1() - line1.getX2());
+        double angle2 = Math.atan2(line2.getY1() - line2.getY2(),
+                                   line2.getX1() - line2.getX2());
+        return Math.toDegrees(angle1-angle2);
+
+    }
+    
 }
